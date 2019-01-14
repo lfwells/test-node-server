d448374119e6179832dd1b1ff3c5f0b3f575b840
lfwells <Lindsay.Wells@utas.edu.au>
2019-01-15 10:42:10 +1100

    Mongodb connection is now back to localhost, meaning you will need monogo db installed with this.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 //Mongo DB
 //mongoose.connect('mongodb://lindsay:W9P,.)C}.$)ArMj5@144.6.226.73:27017/myCoolDB', 	{ useNewUrlParser: true });
-mongoose.connect('mongodb://144.6.226.73:27017/myCoolDB', 	{ useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/myCoolDB', 	{ useNewUrlParser: true });
 //W9P,.)C}.$)ArMj5
 
 const app = express();

8251612861b70a380303fe46a7313aab85ce76fd
lfwells <Lindsay.Wells@utas.edu.au>
2019-01-14 15:25:14 +1100

    Basic mustache configuration, with no caching, used for dev mode.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mustacheExpress = require('mustache-express');
 
 const app = express();
 
 //static file handling
-app.use(express.static("public"));
+app.use(express.static("public"))
+
+//mustache express, and making it not cache
+const mustache = mustacheExpress();
+mustache.cache = null; //not a good idea for production
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
 
 const port = 3000;
 app.listen(port, function()
